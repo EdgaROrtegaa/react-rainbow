8aec7747a1404443fcc796a1ca11bc886e801260
edgar <edgar.jrortega@gmail.com>
2024-07-02 12:28:51 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+
+  
+}
+
 
 function App() {
   return (
@@ -21,5 +35,17 @@ function App() {
     </div>
   );
 }
+
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+
+    return (
+        <div className="App">
+            {colorMap}
+            <ColorForm addColor={addColor} />
+        </div>
+    )
+
 
 export default App;
